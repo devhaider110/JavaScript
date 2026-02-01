f1b8e19062be4ea55d4e718a8831a81f6b7db614
mohd-haider110 <haidermohd872@gmail.com>
2026-02-01 23:55:37 +0530

    String

---
+++ String/string.js
@@ -0,0 +1,28 @@
+const name="haider"
+const repCount = 50
+console.log(`hello my name is ${name} and my repocount is ${repCount}`)
+ const gameName= new String ('haider-hc')
+ console.log(gameName[0]);
+ console.log(gameName.__proto__);
+ console.log(gameName.length);
+ console.log(gameName.toUpperCase());
+ console.log(gameName.charAt(3));;
+ console.log(gameName.indexOf('i'));
+ const newstr = gameName.substring(0,4)
+ console.log(newstr);
+ const anotherstr = gameName.slice(-8,4)
+ console.log(anotherstr);
+ const anotherstr1= "      kjqwdhkjlqwn      j"
+
+ console.log(anotherstr1);
+ console.log(anotherstr1.trim());
+
+ const url="ksjzdhflkjasdnfkjladsbhfn"
+ console.log(url.replace('ksjzdh','haider'));
+console.log(url.includes('dkgjb'));
+
+ 
+                              
+ 
+ 
+ 
